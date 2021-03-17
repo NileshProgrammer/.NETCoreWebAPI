3c808cc65ca2c0d12d0b7889bd9c6f2679cac7b5
Nilesh Rawool <nilesh24rawool@outlook.com>
2021-03-17 12:08:21 +0530

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DepartmentController : ControllerBase
+	{
+		private readonly IConfiguration configuration;
+
+		public DepartmentController(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+
+	/*	[HttpGet]
+
+		public JsonResult Get()
+		{
+			string query = @"
+							select * from department";
+		}
+	*/
+
+	}
+}
